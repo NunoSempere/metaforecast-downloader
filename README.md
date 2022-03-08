97bd605be0842d5a5bf2e856b069833ef1f56744
NunoSempere <nuno.sempere@protonmail.com>
2022-03-08 16:50:41 -0500

    feat: First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## About
+
+This is a node downloader written at the request of Clay Graubard on the 8th of March 2022. It will be maintained until (at least) the 8th of April 2022. But it currently users the metaforecast API, which is in alpha.
+
+## Requirements
+- git
+- node
+
+## Instructions
+
+To install:
+```
+git clone https://github.com/NunoSempere/metaforecast-downloader.git
+cd metaforecast-downloader
+npm install ## not actually necessary, since I've committed the node_modules folder
+```
+
+To run:
+```
+npm run start # or: node index.js
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import fs from "fs";
+
+async function main() {
+  let response = await axios.get(
+    "https://server.loki.red/metaforecast-all-questions"
+  );
+  let data = response.data;
+
+  let ids = ["polymarket-0x515370f1", "metaculus-9584", "predictit-7720"];
+  let questionsOfInterest = data.filter((response) =>
+    ids.includes(response.id)
+  );
+  console.log("Saving results to results.json");
+  fs.writeFileSync("results.json", JSON.stringify(data, null, 4));
+
+  let tsvHeaders = "title\tplatform\tdate\tforecast\n";
+  let tsvRows = questionsOfInterest
+    .map(
+      (question) =>
+        `${question.title}\t${question.platform}\t${
+          question.timestamp
+        }\t${JSON.stringify(question.options)}`
+    )
+    .join("\n");
+  let tsvFile = tsvHeaders + tsvRows;
+  console.log("Saving results to results.tsv");
+  fs.writeFileSync("results.tsv", tsvFile);
+}
+main();
